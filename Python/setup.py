5f87d321559468430a5c0adaaf582316d23e6ff5
Sivert Berg Knudsen <si_vert@hotmail.com>
2021-04-15 16:33:43 +0200

    added files
    setup and loop in python project

---
+++ Python/setup.py
@@ -0,0 +1,6 @@
+# @Author: Sivert Berg Knudsen <sivertbk>
+# @Date:   2021-04-15T16:25:55+02:00
+# @Email:  si_vert@hotmail.com
+# @Filename: setup.py
+# @Last modified by:   sivertbk
+# @Last modified time: 2021-04-15T16:25:55+02:00
