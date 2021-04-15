1b8b8049ae00a79ecf6e119af55dbe7fcab02ca2
Sivert Berg Knudsen <si_vert@hotmail.com>
2021-04-15 17:05:43 +0200

    adding sensor status function

--- Python/plant_status.py
+++ Python/plant_status.py
@@ -5,6 +5,8 @@ Created on Mon Apr 12 14:05:49 2021
 @author: si_ve
 """
 
+from plant_config import *
+
 # Every plant[i] got its own flag-status register with its values,requirements and statuses.
 # Here we update these values
 
@@ -12,7 +14,16 @@ Created on Mon Apr 12 14:05:49 2021
 
 
 # Get values from soil_sensor and check status
-def sensor_value(sensor):
+def plant_sensor_status(plant_name, sensor_name):
+    if sensor_name == 'soil':
+        sensor_name = 'soil_value'
+        if plant[plant_name][str(sensor_name)] < plant[plant_name]['water_requirement']:
+            status = 'need water'
+        else:
+            status = 'im good'
+    return status
+
+print(plant_sensor_status(0,'soil'))
     # CoT get values
         # Get value from CoT too soil_sensor_value(plant)
         # update time and value for last_soil_measure{time:datetime, soil:value}
