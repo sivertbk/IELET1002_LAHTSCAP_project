dabae4d0c9555f459b7c0c3352688072867d46fd
Sivert Berg Knudsen <si_vert@hotmail.com>
2021-04-16 02:17:54 +0200

    first attempt at timestamp function
    change plant[i] to string(not done). Add function for timestamp(not done). + testing changes and git fuctionality

--- Python/setup.py
+++ Python/setup.py
@@ -2,10 +2,12 @@
 # @Date:   2021-04-15T16:25:55+02:00
 # @Email:  si_vert@hotmail.com
 # @Filename: setup.py
-# @Last modified by:   sivertbk
-# @Last modified time: 2021-04-15T16:43:59+02:00
+# @Last modified by:
+# @Last modified time: 2021-04-16T00:36:48+02:00
 
 
 # Here we initals the script.
 
 # Start with sending a signal to ESP32 that the system has startet and is going in setup-mode
+#not quite sure here ?
+#testing changes... ignore
