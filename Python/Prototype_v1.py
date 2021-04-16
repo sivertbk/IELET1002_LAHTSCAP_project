7a8dd7539c49d2945ec91b2e26d210a6def480df
Paal <80708206+PauloNTNU@users.noreply.github.com>
2021-04-16 20:30:37 +0200

    Some description edits & Added a python file
    Python file is to be used to create first draft of how the codeflow would work.

---
+++ Python/Prototype_v1.py
@@ -0,0 +1,24 @@
+"""
+This file contains codeflow suggestion for how the codeflow would work in
+the project.
+*Add more text*
+"""
+## Get class module COT_Signal for communication
+from CoT import COT_Signal
+token = "Insert token"
+
+## Keys
+soilsensor_key = COT_Signal()
+temperature_key = COT_Signal()
+humidity_key = COT_Signal()
+uvsensor_key = COT_Signal()
+luxsensor_key = COT_Signal()
+pump_state_key = COT_Signal()
+ultrasonic_key = COT_Signal()
+
+def watering():
+    ## Here is where it'll pump water to flower if needed based on soil sensor
+    print("Water Water")
+
+while True:
+    print("Yo")
