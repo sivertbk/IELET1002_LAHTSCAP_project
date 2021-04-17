39be5b391349e9f26d3a008d3835cf8eb7486f42
Paal <80708206+PauloNTNU@users.noreply.github.com>
2021-04-17 17:58:52 +0200

    Some updates

--- Python/potential_future_build/Prototype_v2.py
+++ Python/potential_future_build/Prototype_v2.py
@@ -8,23 +8,39 @@ from plant_modules import COT_Signal
 token1 = "Insert token"
 token2 = "Insert token"
 
-## Keys
-# soilsensor_key1 = COT_Signal()
-# temperature_key1 = COT_Signal()
-# humidity_key1 = COT_Signal()
-# uvsensor_key1 = COT_Signal()
-# luxsensor_key1 = COT_Signal()
-# pump_state_key1 = COT_Signal()
-# ultrasonic_key1 = COT_Signal()
+## Keys for token1
+soilsensor_key1 = COT_Signal()
+temperature_key1 = COT_Signal()
+humidity_key1 = COT_Signal()
+uvsensor_key1 = COT_Signal()
+luxsensor_key1 = COT_Signal()
+pump_state_key1 = COT_Signal()
+ultrasonic_key1 = COT_Signal()
+
+## Keys for token2
+soil_requirement_key2 = COT_Signal()
 
 
 def plant_calibration():
     """
     To calibrate the system to fit for the plant the code is currently working with. 
+    Should be able to update the dictionary & save new configuration. 
     """
     print("This is fine")
     
-    
+def watering():
+    soilsensor_value  = soilsensor_key1.get()["Value"]
+    if soilsensor_value < soil_requirement_key2.get()["Value"]: # and last time watering happened is more than 30 min
+        pump_state_key1.put(1)
+    else:
+        print("Too soon")
 
+def plant_lights():
+    """
+    If the plants haven't got enough sunlight, then it should turn on the plant lights
+    """
+    
 while True:
-    print("Yo")
+    """
+    Systems main code to be run. 
+    """
--- Python/potential_future_build/plant_config_v2.py
+++ Python/potential_future_build/plant_config_v2.py
@@ -2,13 +2,15 @@
 This file is used for a plants configuration.
 *Add more text*
 '''
+
 import datetime
 from plant_modules import COT_Signal
 
 token2 = 'Insert token'
+
 new_plant_key2 = COT_Signal()
 plant_number_key2 = COT_Signal()
-water_requirement_key2 = COT_Signal()
+soil_requirement_key2 = COT_Signal()
 light_requirement_key2 = COT_Signal()
 temperature_maximum_key2 = COT_Signal()
 temperature_minimum_key2 = COT_Signal()
@@ -18,10 +20,11 @@ humidity_requirement_key2 = COT_Signal()
 
 """
 plant variable is a dictionary that contains a dictionary of a plants configuration.
-Here one can store 
+Here one can store new configuration for new plants and get already existing configuration for plants that falls
+within the same category.
 """
 plant = {'0':
-         {'water_requirement':water_requirement_key2.get()['Value'],
+         {'soil_requirement':soil_requirement_key2.get()['Value'],
           'light_requirement':light_requirement_key2.get()['Value'],
           'temperature_maximum':temperature_maximum_key2.get()['Value'],
           'temperature_minimum':temperature_minimum_key2.get()['Value'],
