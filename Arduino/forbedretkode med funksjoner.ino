c343a401d5d44b625d711f61e552d4d04ea60af1
Pederbs <pederbsa@stud.ntnu.no>
2021-04-21 17:53:14 +0200

    Update forbedretkode med funksjoner.ino
    kommentert litt og ryddet opp i variabler (sa jeg at vi ikke trenger noen form for Globale variabler?)
    RTOS'en kjører fint uten globale variabler tror dette kan være fordi den fungerer som en slags void loop og derfor har samme "rettigheter" som void loop (blir behandlet snillere enn vanlige funksjoner

--- Arduino/forbedretkode med funksjoner.ino
+++ Arduino/forbedretkode med funksjoner.ino
@@ -33,9 +33,8 @@ const int led_pin = 14;
 const int pump_channel = 5;
 const int frequency = 1000;
 const int resolution = 8;
-const int pump_magnitude = 255;
 
-// Global Variables
+// Value Variables
 int soilsensor_percent;
 int uvsensor_percent;
 float humidity_value;
@@ -44,29 +43,31 @@ float lux_value;
 float distance_value;
 int pump_state;
 
-// Loop Variables
+// Long Variables
 //unsigned long duration;
-int distance;
-unsigned long pump_duration;
-unsigned long wait_duration = 3000;
-unsigned long wait_start = 0;
-bool pump_active = 0;
-int pump_strenght = 0;
-
 //unsigned long time;
+unsigned long pump_duration;      // Given in ms and determines how long the pump should run
 unsigned long pump_start = 0;
+unsigned long wait_start = 0;
+unsigned long wait_duration = 3000;
 
-Adafruit_AHTX0 aht; 
-DFRobot_VEML7700 veml;
-CircusESP32Lib circusESP32(server, ssid, psk);
-TaskHandle_t Task1;
+// Pump Logic Variables
+bool pump_active = 0;             // Bool variable that determines if the pump is active or not
+int pump_strenght = 0;            // Variable to determine how fast the pump should run
+const int pump_magnitude = 255;   // Constant to say how fast the pump should run when it is running (given in 8 bits)
+
+
+Adafruit_AHTX0 aht;                             // Defines the function used to retrive temp and humi
+DFRobot_VEML7700 veml;                          // Defines the function used to retrive Lux
+CircusESP32Lib circusESP32(server, ssid, psk);  // Defines the function used to communicate to CoT
+TaskHandle_t Task1;                             // etteller annet RTOS relevant Pål kan det
 
 void setup(){
 
   //Start communication
   Serial.begin(115200);
-  veml.begin();     //starter funksjonen for å lese lysstyrke av Lux sensoren
-  aht.begin();      //starter funksjon for å lese temperatur og romfuktighet
+  aht.begin();                    // Starts the function to retreve temp and humid
+  veml.begin();                   // Starts the function to retreve Lux value
   circusESP32.begin();
   
   // pinModes
@@ -74,6 +75,9 @@ void setup(){
   pinMode(uvsensor_pin, INPUT);
   pinMode(echo_pin, INPUT);
   pinMode(trigger_pin, OUTPUT);
+  pinMode(pump_pin, OUTPUT);
+  pinMode(led_pin, OUTPUT);
+  
 
   //ledC Config
   ledcSetup(pump_channel, frequency, resolution);
@@ -91,6 +95,7 @@ void setup(){
 
 }
 
+// RTOS
 // Runs in Core 0, and is used for all COT communication
 void communication (void * pvParameters){
   for(;;){
@@ -101,8 +106,9 @@ void communication (void * pvParameters){
     circusESP32.write(luxsensor_key1, lux_value, token1);
     circusESP32.write(ultrasonic_key1, distance_value, token1);
 
-    pump_state = circusESP32.read(pump_state_key1 , token1);
-
+    pump_state = circusESP32.read(pump_state_key1 , token1);    // Reads the state of the pump to know when to water the plant
+    
+    // Used to time how long a cycle takes to finish
     Serial.println("");
     Serial.println("");
     Serial.println("      ALLE VERDIER HENTET");
@@ -113,16 +119,14 @@ void communication (void * pvParameters){
 
 int get_soil(int pin){
   int value = analogRead(pin);
-  int soilsensor_percent = map(value,0,4095,0,100);    // finner den prosentvise verdien for jordfuktighet
-//  int soilsensor_persent = (value/4095)*100;  // finner den prosentvise verdien for jordfuktighet  
+  int soilsensor_percent = map(value,0,4095,0,100);// finner den prosentvise verdien for jordfuktighet
   Serial.println(); Serial.print("                Soil Sensor in %: "); Serial.println(soilsensor_percent);
   return soilsensor_percent;
 }
 
 int get_uv(int pin){
-  int value = analogRead(pin); // denne viser for detmeste 0 men om den får direkte sollys gir den verdier (16.04 kl 14ish ga den 400 ved direkte sollys)
-  int value_percent = map(value,0,611,0,100);         // usikker på nevner verdien, kan være høyere // hyeste målt verdi 1300 20.04 611
-//  int uvsensor_persent = (uvsensor_value/611)*100;       // usikker på nevner verdien, kan være høyere
+  int value = analogRead(pin);                    // denne viser for detmeste 0 men om den får direkte sollys gir den verdier (16.04 kl 14ish ga den 400 ved direkte sollys)
+  int value_percent = map(value,0,611,0,100);     // usikker på nevner verdien, kan være høyere // hyeste målt verdi 1300 20.04 611
   Serial.print("                UV in %: "); Serial.println(value_percent);
   return value_percent;
 }
@@ -149,7 +153,7 @@ float get_lux(){
   veml.getALSLux(lux_value);
   Serial.print("                Lux:"); Serial.print(lux_value); Serial.println(" lx");
   return lux_value;
-//  lux_percent = map(lux_value,0,FYLL INN,0,100); // har fått lux = 34 6033.88lx med lys av mobil
+//  lux_percent = map(lux_value,0,FYLL INN,0,100); // har fått lux = 274,000lx (ish) med lys av mobil  IKKE RELEVANT
 }
 
 float get_temp(){
@@ -188,7 +192,7 @@ void loop(){
     Serial.println(temperature_value);
     Serial.println(humidity_value);
 */    
-    Serial.println(pump_state);
+    Serial.print("Pump state = "); Serial.println(pump_state);
   
     if (pump_active == 0) {
       switch (pump_state){
