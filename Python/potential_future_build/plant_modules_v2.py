3d826b863678aaad64e6d540cf0341d8df48ec84
Sivert Berg Knudsen <si_vert@hotmail.com>
2021-04-21 15:08:37 +0200

    Update plant_modules_v2.py
    plant to plant_dictionary

--- Python/potential_future_build/plant_modules_v2.py
+++ Python/potential_future_build/plant_modules_v2.py
@@ -69,7 +69,7 @@ def update_plant_soil_value(plant_name):
     """
     # list of all soil keys for each plant, where the index matches all the plant names.
     plant_soil_keys = [soil_0_key, soil_1_key, soil_2_key, soil_3_key, soil_4_key, soil_5_key, soil_6_key, soil_7_key]
-    plant[str(plant_name)]['soil_value'] = plant_soil_keys[int(plant_name)].get()['Value']
+    plant_dictionary[str(plant_name)]['soil_value'] = plant_soil_keys[int(plant_name)].get()['Value']
     return
 
 
@@ -81,9 +81,9 @@ def update_plant_water_state(plant_name):
     """
     plant_pump_keys = [pump_0_key, pump_1_key, pump_3_key, pump_4_key, pump_5_key, pump_6_key, pump_7_key]
     if plant_pump_keys[int(plant_name)].get()['Value'] == 1:
-        plant[str(plant_name)]['water'] = True
+        plant_dictionary[str(plant_name)]['water'] = True
     elif plant_pump_keys[int(plant_name)].get()['Value'] == 0:
-        plant[str(plant_name)]['water'] = False
+        plant_dictionary[str(plant_name)]['water'] = False
     return
 
 
@@ -95,9 +95,9 @@ def plant_soil_check(plant_name):
     When the plant need water it changes the plants water status to True
     """
     current_time = int(time.time()) # current time in epoch
-    soil_value = plant[str(plant_name)]['soil_value']
-    Threshold = plant[str(plant_name)]['water_requirement']
-    last_water = plant[str(plant_name)]['last_water']
+    soil_value = plant_dictionary[str(plant_name)]['soil_value']
+    Threshold = plant_dictionary[str(plant_name)]['water_requirement']
+    last_water = plant_dictionary[str(plant_name)]['last_water']
     water_interval = 10 # 12 Hours interval = 43200 seconds(25 seconds offset/lag)
     control_wait_time = 30 # control wait time 30 minutes = 1800 seconds
     #global soil_control, soil_time_tracker
@@ -108,7 +108,7 @@ def plant_soil_check(plant_name):
             if (current_time - soil_time_tracker[str(plant_name)]) > control_wait_time:
                 soil_control[str(plant_name)] = False
                 soil_time_tracker[str(plant_name)] = 0
-                plant[str(plant_name)]['water'] = True
+                plant_dictionary[str(plant_name)]['water'] = True
                 return print('watering plant',str(plant_name)+'!')
             else:
                 return
@@ -128,8 +128,8 @@ def plant_soil_check(plant_name):
 
 def water(plant_name):
     plant_pump_keys = [pump_0_key, pump_1_key, pump_3_key, pump_4_key, pump_5_key, pump_6_key, pump_7_key]
-    if plant[str(plant_name)]['water']:
-        plant[str(plant_name)]['last_water'] = int(time.time())
+    if plant_dictionary[str(plant_name)]['water']:
+        plant_dictionary[str(plant_dictionary_name)]['last_water'] = int(time.time())
         return plant_pump_keys[int(plant_name)].put(1)
     else:
         return
