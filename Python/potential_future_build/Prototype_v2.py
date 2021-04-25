e0b25b4732f0ae226d0d93ec11e1c6324bb90cb9
Sivert Berg Knudsen <si_vert@hotmail.com>
2021-04-25 19:30:14 +0200

    Update Prototype_v2.py

--- Python/potential_future_build/Prototype_v2.py
+++ Python/potential_future_build/Prototype_v2.py
@@ -1,5 +1,5 @@
 # @Date:   2021-04-21T14:03:41+02:00
-# @Last modified time: 2021-04-25T19:13:52+02:00
+# @Last modified time: 2021-04-25T19:29:51+02:00
 
 
 
@@ -27,37 +27,43 @@ if __name__ == "__main__":
         new_plant = CoT.new_plant_configuration_key2.get()['Value']
         save_configuration = CoT.save_configuration_key2.get()['Value']
 
-        # Will be true if user wants to switch plant or create a new one. It will also be run if plant_dictionary does not have any dictionary
-        if new_plant == 1 or plant_dictionary == 1:
+        # Whenever user wants to store a new configuration or look at what's already stored.
+        if (new_plant == 1):
             plant_dictionary = plant_modules.plant_setup()
 
-            plant_dictionary = plant_modules.update_plant_sensor_values(plant_dictionary, plant_name)
-
-            plant_dictionary = plant_modules.update_plant_input_states_state(plant_dictionary, plant_name)
+        # Will be true if user wants to save new configuration.
+        if (save_configuration == 1):
+            plant_number = str(CoT.plant_number_key2.get()['Value'])
+            plant_modules.plant_configuration(plant_number, plant_dictionary[plant_number])
 
         #### Update sensor values ####--------------------------------------------------------------------------------------
-        for plant_name in range(0, 1): # When done: range(0, len(plant_dictionary)): It wil then run trough all the plants
-            plant_modules.update_plant_soil_value(plant_name)
-            plant_modules.update_plant_water_state(plant_name)
+        for plant_name in range(1, 9):
+            if plant_dictionary[str(plant_name)]['active_status']: # Only run if plant is active
+                plant_dictionary = plant_modules.update_plant_sensor_values(plant_dictionary, plant_name)
+                plant_dictionary = plant_modules.update_plant_input_states_state(plant_dictionary, plant_name)
 
-            #### Check sensors ####-----------------------------------------------------------------------------------------
+                #### Check sensors ####-----------------------------------------------------------------------------------------
 
-            plant_modules.plant_soil_check(plant_name)
+                plant_modules.plant_soil_check(plant_name)
 
-            #### Water and light ####---------------------------------------------------------------------------------------
-            plant_modules.water(plant_name)
+                #### Water and light ####---------------------------------------------------------------------------------------
+                plant_modules.water(plant_name)
 
 
 
         #### Serial monitor ####--------------------------------------------------------------------------------------------
         print('##########################################################')
-        print('PLANT STATUSES:')
-        for plant_name in range(0, 2):
+        print('PLANT STATUSES:                           CLOCK:', datetime.fromtimestamp(int(time.time())).strftime('%H:%M:%S'))
+        for plant_name in range(1, 3):
             print('\n  Plant', str(plant_name) +':')
-            print('  Soil:', int(plant_dictionary[str(plant_name)]['soil_value']), '    Threshold:', plant_dictionary[str(plant_name)]['water_requirement'])
+            print('  Soil:', int(plant_dictionary[str(plant_name)]['soil_value']), '    Threshold:', int(plant_dictionary[str(plant_name)]['soil_requirement']))
             print('  Pump:', plant_dictionary[str(plant_name)]['water'], '  Last given water:', datetime.fromtimestamp(plant_dictionary[str(plant_name)]['last_water']).strftime('%H:%M:%S %d/%m-%Y'))
 
         print('##########################################################')
+
+
+        with open('plant_dictionaries_v2.json', 'w') as json_file:
+            json.dump(plant_dictionary, json_file)
 
 
 
